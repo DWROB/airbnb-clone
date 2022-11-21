7927df3442971998f7cd6b3e8377379678cc1d7f
Dal Robinson <dalwrobinson@protonmail.com>
2022-11-21 15:30:54 +0000

    all three models created and migrated

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  has_many :apartments, dependent: :destroy
+  has_many :reservations, dependent: :destroy
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
