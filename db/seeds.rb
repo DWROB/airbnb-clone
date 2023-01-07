dd5bf3c00191fee6f5fb8d929682b2cd59d85cc3
Dal Robinson <dalwrobinson@protonmail.com>
2023-01-07 12:47:23 +0000

    reseeded database with valid images + 8 default users.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,74 +1,94 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
-
 CITIES = %i[lisbon barcelona paris london]
 counter_seed = 0
 IMAGESINTERIOR1 = [
-  "https://res.cloudinary.com/dk2w7ocuc/image/upload/v1669214027/interior_apartment/minh-pham-OtXADkUh3-I-unsplash_zmnshz.jpg",
-  "https://res.cloudinary.com/dk2w7ocuc/image/upload/v1669214021/interior_apartment/armin-djuhic-mcL2f-J74GY-unsplash_aiuron.jpg"]
+  "https://res.cloudinary.com/dk2w7ocuc/image/upload/v1673094311/interiors/alisha-hieb-qly35FEQwA4-unsplash_qdmpts.jpg",
+  "https://res.cloudinary.com/dk2w7ocuc/image/upload/v1673094309/interiors/christian-lambert-hlOpCML8twI-unsplash_uxfdgs.jpg",
+  "https://res.cloudinary.com/dk2w7ocuc/image/upload/v1673094309/interiors/zac-gudakov-_IPJtJJ9fig-unsplash_g1mjes.jpg",
+  "https://res.cloudinary.com/dk2w7ocuc/image/upload/v1673094302/interiors/taylor-heery-8DlbPCxfGHA-unsplash_h0y3k7.jpg"
+  ]
   IMAGEINTERIOR2 = [
-  "https://res.cloudinary.com/dk2w7ocuc/image/upload/v1669214015/interior_apartment/jason-briscoe-UV81E0oXXWQ-unsplash_qwybki.jpg",
-  "https://res.cloudinary.com/dk2w7ocuc/image/upload/v1669214005/interior_apartment/patrick-perkins-3wylDrjxH-E-unsplash_sshba2.jpg"
+    "https://res.cloudinary.com/dk2w7ocuc/image/upload/v1673094308/interiors/zac-gudakov-vM41GJbkPeM-unsplash_imjhqr.jpg",
+    "https://res.cloudinary.com/dk2w7ocuc/image/upload/v1673094307/interiors/hannah-busing-U-k6XLlml1I-unsplash_pirpp2.jpg",
+    "https://res.cloudinary.com/dk2w7ocuc/image/upload/v1673094308/interiors/joseph-albanese-xx0oSB1YxRE-unsplash_odnn0i.jpg",
+    "https://res.cloudinary.com/dk2w7ocuc/image/upload/v1673094302/interiors/patrick-perkins-G3qlZQXsBOE-unsplash_vndbm2.jpg"
   ]
   IMAGEINTERIOR3 = [
-  "https://res.cloudinary.com/dk2w7ocuc/image/upload/v1669213994/interior_apartment/patrick-perkins-3wylDrjxH-E-unsplash_km5aah.jpg",
-  "https://res.cloudinary.com/dk2w7ocuc/image/upload/v1669126199/interior_apartment/francesca-tosolini-qnSTxcs0EEs-unsplash_h4ohpg.jpg"
+    "https://res.cloudinary.com/dk2w7ocuc/image/upload/v1673094307/interiors/grovemade-vfIx29EsLHA-unsplash_dy8rsb.jpg",
+    "https://res.cloudinary.com/dk2w7ocuc/image/upload/v1673094308/interiors/henry-mcintosh-HYxR4XN8ZFE-unsplash_q3weqe.jpg",
+    "https://res.cloudinary.com/dk2w7ocuc/image/upload/v1673094307/interiors/christian-mackie-SxBca4GcC9k-unsplash_fjxlqm.jpg",
+    "https://res.cloudinary.com/dk2w7ocuc/image/upload/v1673094301/interiors/med-badr-chemmaoui-xtDpXi_a-YQ-unsplash_v6efao.jpg"
   ]
   IMAGEINTERIOR4= [
-  "https://res.cloudinary.com/dk2w7ocuc/image/upload/v1669126198/interior_apartment/rhema-kallianpur-jbJ-_hw2yag-unsplash_g0iuba.jpg",
-  "https://res.cloudinary.com/dk2w7ocuc/image/upload/v1669126198/interior_apartment/rhema-kallianpur-jbJ-_hw2yag-unsplash_g0iuba.jpg"
+    "https://res.cloudinary.com/dk2w7ocuc/image/upload/v1673094306/interiors/ralph-ravi-kayden-FqqiAvJejto-unsplash_h7znwt.jpg",
+    "https://res.cloudinary.com/dk2w7ocuc/image/upload/v1673094306/interiors/collov-home-design-SrioT6tdWII-unsplash_ab1cja.jpg",
+    "https://res.cloudinary.com/dk2w7ocuc/image/upload/v1673094306/interiors/grant-UhpYKnqZwE8-unsplash_geupdf.jpg",
+    "https://res.cloudinary.com/dk2w7ocuc/image/upload/v1673094301/interiors/huy-nguyen-9vvp_nuVaJk-unsplash_pju3aj.jpg"
   ]
   IMAGEINTERIOR5= [
-    "https://res.cloudinary.com/dk2w7ocuc/image/upload/v1669126198/interior_apartment/rhema-kallianpur-jbJ-_hw2yag-unsplash_g0iuba.jpg",
-    "https://res.cloudinary.com/dk2w7ocuc/image/upload/v1669112360/interior_apartment/outsite-co-R-LK3sqLiBw-unsplash_oznuy3.jpg"
+    "https://res.cloudinary.com/dk2w7ocuc/image/upload/v1673094305/interiors/emily-wang-Wv65tpVIdDg-unsplash_rdmnml.jpg",
+    "https://res.cloudinary.com/dk2w7ocuc/image/upload/v1673094304/interiors/andrea-davis-7450hH--84M-unsplash_xdp3kj.jpg",
+    "https://res.cloudinary.com/dk2w7ocuc/image/upload/v1673094305/interiors/christopher-lemercier-tWPDKhDaE4k-unsplash_lgpslq.jpg",
+    "https://res.cloudinary.com/dk2w7ocuc/image/upload/v1673094300/interiors/francesca-tosolini-tHkJAMcO3QE-unsplash_1_ww0rzm.jpg"
   ]
-IMAGESURL = [["https://res.cloudinary.com/dk2w7ocuc/image/upload/v1669126201/exterior_apartment/micah-carlson-qN-T-pY17Vc-unsplash_lpbfln.jpg", IMAGESINTERIOR1],
-  ["https://res.cloudinary.com/dk2w7ocuc/image/upload/v1669126201/exterior_apartment/stephen-leonardi-XKIO6ZgCObo-unsplash_m9pru7.jpg", IMAGEINTERIOR2],
-  ["https://res.cloudinary.com/dk2w7ocuc/image/upload/v1669126201/exterior_apartment/chi-m-R1uiDu8vBh0-unsplash_i1cb1b.jpg", IMAGEINTERIOR3 ],
-  ["https://res.cloudinary.com/dk2w7ocuc/image/upload/v1669126199/exterior_apartment/vu-anh-7hHTOtSMTKs-unsplash_qy7i66.jpg", IMAGEINTERIOR4 ],
-  ["https://res.cloudinary.com/dk2w7ocuc/image/upload/v1669126198/exterior_apartment/diego-carneiro-w4XtcQsBVeI-unsplash_tb2iwa.jpg" ,IMAGEINTERIOR5]
-
-]
+  IMAGEINTERIOR6 = [
+    "https://res.cloudinary.com/dk2w7ocuc/image/upload/v1673094305/interiors/point3d-commercial-imaging-ltd-vkniNktOaIs-unsplash_ymkzmj.jpg",
+    "https://res.cloudinary.com/dk2w7ocuc/image/upload/v1673094304/interiors/kari-shea-MfJ9g64-WxQ-unsplash_eii6we.jpg",
+    "https://res.cloudinary.com/dk2w7ocuc/image/upload/v1673094304/interiors/ralph-ravi-kayden-JDBVXignFdA-unsplash_uqgwla.jpg",
+    "https://res.cloudinary.com/dk2w7ocuc/image/upload/v1673094300/interiors/deborah-cortelazzi-gREquCUXQLI-unsplash_efiug2.jpg"
+  ]
+  IMAGEINTERIOR7 = [
+    "https://res.cloudinary.com/dk2w7ocuc/image/upload/v1673094303/interiors/antonio-caverzan-D1YruV0KUDw-unsplash_ejssrm.jpg",
+    "https://res.cloudinary.com/dk2w7ocuc/image/upload/v1673094303/interiors/sidekix-media-JF5IuDNxN6M-unsplash_mwbo4z.jpg",
+    "https://res.cloudinary.com/dk2w7ocuc/image/upload/v1673094303/interiors/roxanne-joncas-DxLGT3bEKkE-unsplash_bbostz.jpg",
+    "https://res.cloudinary.com/dk2w7ocuc/image/upload/v1673094301/interiors/naomi-hebert-MP0bgaS_d1c-unsplash_bugo9u.jpg"
+  ]
+  IMAGEINTERIOR8 = [
+    "https://res.cloudinary.com/dk2w7ocuc/image/upload/v1673094303/interiors/keren-fedida-ZXZ7t7TneQs-unsplash_vtpgwn.jpg",
+    "https://res.cloudinary.com/dk2w7ocuc/image/upload/v1673094302/interiors/nathan-van-egmond-9LMRQdVv7hw-unsplash_d46dxe.jpg",
+    "https://res.cloudinary.com/dk2w7ocuc/image/upload/v1673094303/interiors/francesca-tosolini-21xbUDIN8ao-unsplash_oojb45.jpg",
+    "https://res.cloudinary.com/dk2w7ocuc/image/upload/v1673094302/interiors/nathan-van-egmond-9LMRQdVv7hw-unsplash_d46dxe.jpg"
+  ]
+IMAGESURL = [["https://res.cloudinary.com/dk2w7ocuc/image/upload/v1673093669/Exteriors/babak-habibi-hZyN1JsDOLk-unsplash_r0jisu.jpg", IMAGESINTERIOR1],
+  ["https://res.cloudinary.com/dk2w7ocuc/image/upload/v1673093667/Exteriors/marija-zaric-cz75BNrAzOs-unsplash_uqcfjs.jpg", IMAGEINTERIOR2],
+  ["https://res.cloudinary.com/dk2w7ocuc/image/upload/v1673093667/Exteriors/philippe-gauthier-mlE5f4rcIqM-unsplash_jnxzyo.jpg", IMAGEINTERIOR3 ],
+  ["https://res.cloudinary.com/dk2w7ocuc/image/upload/v1673093667/Exteriors/waldemar-brandt-7ILCExKUPqU-unsplash_maet7f.jpg", IMAGEINTERIOR4 ],
+  ["https://res.cloudinary.com/dk2w7ocuc/image/upload/v1673093568/Exteriors/l-hajperlink-rySNgIeB8wA-unsplash_ys19ot.jpg" ,IMAGEINTERIOR5],
+  ["https://res.cloudinary.com/dk2w7ocuc/image/upload/v1673093565/Exteriors/tanya-kukarkina-U2Pt1z0RKGs-unsplash_atmqyw.jpg", IMAGEINTERIOR6],
+  ["https://res.cloudinary.com/dk2w7ocuc/image/upload/v1673093565/Exteriors/tanya-kukarkina-U2Pt1z0RKGs-unsplash_atmqyw.jpg", IMAGEINTERIOR7],
+  ["https://res.cloudinary.com/dk2w7ocuc/image/upload/v1673093564/Exteriors/brandon-griggs-wR11KBaB86U-unsplash_jbuefe.jpg", IMAGEINTERIOR8]]
 puts "deleting records"
 User.destroy_all
 
 puts "making new seeds"
 
-
-
 IMAGESURL.each do |image|
-    user = User.new(
+  user = User.new(
     first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name,
     email: Faker::Internet.email,
     rating: rand(1..5).floor,
     password: Faker::Internet.password(min_length: 8),
     host: true
-      )
-    puts user
-    user.save
-    counter_seed += 1
+  )
+  puts user
+  user.save
+  counter_seed += 1
 
-    apartment = Apartment.new(
-      address: CITIES.sample,
-      rating: rand(1..5).floor,
-      user_id: user.id,
-      title: Faker::Lorem.sentence(word_count: 4),
-      description: Faker::Lorem.sentence(word_count: 90)
-    )
-    img_file = URI.open(image[0])
+  apartment = Apartment.new(
+    address: CITIES.sample,
+    rating: rand(3..5).floor,
+    user_id: user.id,
+    title: Faker::Lorem.sentence(word_count: 4),
+    description: Faker::Lorem.sentence(word_count: 90)
+  )
+  img_file = URI.open(image[0])
+  apartment.photos.attach(io: img_file, filename: "main_image.jpg", content_type: "image/jpg")
+  apartment.save
+  image[1].each do |sub_image|
+    img_file = URI.open(sub_image)
     apartment.photos.attach(io: img_file, filename: "main_image.jpg", content_type: "image/jpg")
-    apartment.save
-    image[1].each do |sub_image|
-      img_file = URI.open(sub_image)
-      apartment.photos.attach(io: img_file, filename: "main_image.jpg", content_type: "image/jpg")
-    end
-    booking = Booking.new(
+  end
+  booking = Booking.new(
     start_date: Faker::Date.between(from: '2023-01-23', to: '2023-09-25'),
     end_date: Faker::Date.between(from: '2022-11-23', to: '2023-09-25'),
     user_id: user.id,
